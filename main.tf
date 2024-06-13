6cc2a588a3c37be77216995cc909f8581f07e90f
imorshed1 <ilia.morshed@gmail.com>
2024-06-13 13:02:19 -0400

    Deleted http

--- main.tf
+++ main.tf
@@ -69,13 +69,7 @@ module "alb" {
     }  
   ]  
   
-  http_tcp_listeners = [  
-    {  
-      port               = 80  
-      protocol           = "HTTP"  
-      target_group_index = 0  
-    }  
-  ]  
+
   
   tags = {  
     Environment = "dev"  
