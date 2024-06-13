5e655e78c7a403f1dbe5e21cc7ad37946c772a1d
imorshed1 <ilia.morshed@gmail.com>
2024-06-13 14:14:27 -0400

    update refactoring

--- main.tf
+++ main.tf
@@ -22,50 +22,46 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"  
   
   name = var.environment.name  
-  cidr = "${var.enviorment.network_prefix}.0.0/16"  
+  cidr = "${var.environment.network_prefix}.0.0/16"  
   
   azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]  
-  public_subnets = ["${var.enviorment.network_prefix}.101.0/24", "${var.enviorment.network_prefix}.102.0/24", "${var.enviorment.network_prefix}.103.0/24"]  
+  public_subnets = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]  
   
   tags = {  
-    Terraform    = "true"  
-    Environment  = var.enviorment.name 
+    Terraform   = "true"  
+    Environment = var.environment.name  
   }  
 }  
   
-
 module "autoscaling" {  
   source  = "terraform-aws-modules/autoscaling/aws"  
   version = "7.6.0"  
-    
-  name                = "${var.enviorment.name}-blog"  
-  min_size            = var.min_size
-  max_size            = var.max_size
+  
+  name                = "${var.environment.name}-blog"  
+  min_size            = var.min_size  
+  max_size            = var.max_size  
   
   vpc_zone_identifier = module.blog_vpc.public_subnets  
   target_group_arns   = module.blog_alb.target_group_arns  
   security_groups     = [module.blog_sg.security_group_id]  
   
-  image_id            = data.aws_ami.app_ami.id    
-  instance_type       = var.instance_type  
+  image_id      = data.aws_ami.app_ami.id  
+  instance_type = var.instance_type  
 }  
-
   
 module "blog_alb" {  
-  source = "terraform-aws-modules/alb/aws" 
-  version = "~> 6.0" 
+  source  = "terraform-aws-modules/alb/aws"  
+  version = "~> 6.0"  
   
-  name                = "${var.enviorment.name}-blog-alb"  
-
+  name                = "${var.environment.name}-blog-alb"  
   load_balancer_type  = "application"  
-
   vpc_id              = module.blog_vpc.vpc_id  
   subnets             = module.blog_vpc.public_subnets  
   security_groups     = [module.blog_sg.security_group_id]  
   
   target_groups = [  
     {  
-      name_prefix       = "${var.enviorment.name}"  
+      name_prefix       = "${var.environment.name}"  
       backend_protocol  = "HTTP"  
       backend_port      = 80  
       target_type       = "instance"  
@@ -81,15 +77,14 @@ module "blog_alb" {
   ]  
   
   tags = {  
-    Environment = var.enviorment.name  
+    Environment = var.environment.name  
   }  
 }  
-
   
 module "blog_sg" {  
   source  = "terraform-aws-modules/security-group/aws"  
   version = "5.1.2"  
-  name    = "${var.enviorment.name}-blog"  
+  name    = "${var.environment.name}-blog"  
   
   vpc_id              = module.blog_vpc.vpc_id  
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]  
